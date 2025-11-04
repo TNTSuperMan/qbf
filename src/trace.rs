f10d2b3a9d6575e83bda697531fd013f4489a4b4
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-04 18:59:32 +0900

    ちょっとトレスを修正

--- src/trace.rs
+++ src/trace.rs
@@ -9,7 +9,7 @@ pub fn instructions_to_string(instructions: Vec<Instruction>) -> String {
             Instruction::MulAndSetZero(src, _dests) => format!("MulAndSetZero(src: {})",src),
             Instruction::Out(ptr) => format!("Out(to: {})", ptr),
             Instruction::In(ptr) => format!("In(to: {})", ptr),
-            Instruction::LoopStart(addr, ptr, is_stable) => format!("LoopStart(addr: {}, ptr{}, {})", addr, ptr, if is_stable { "stable" } else { "unstable" }),
+            Instruction::LoopStart(addr, ptr, is_stable) => format!("LoopStart(addr: {}, ptr: {}, {})", addr, ptr, if is_stable { "stable" } else { "unstable" }),
             Instruction::LoopEnd(addr, ptr, is_stable) => format!("LoopEnd(addr: {}, ptr: {}, {})", addr, ptr, if is_stable { "stable" } else { "unstable" }),
         });
     }
