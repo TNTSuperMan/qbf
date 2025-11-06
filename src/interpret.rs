9620047204731ffff63ae5daf89b57d28d7e2c0c
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-06 23:59:23 +0900

    共通で計算するやつをまとめる

--- src/interpret.rs
+++ src/interpret.rs
@@ -1,55 +1,51 @@
 use crate::{io::{input, output}, parser::{InstOp, Instruction}, vm::BFVM};
 
 pub fn step(vm: &mut BFVM) {
-    let Instruction { opcode, pointer: ptr } = &vm.insts[vm.pc];
+    let Instruction { opcode, pointer } = &vm.insts[vm.pc];
+    let ptr = (pointer + vm.offset) as usize;
     match opcode {
         InstOp::Breakpoint => {
             // 標準出力と分けるだけ、エラーじゃない
-            eprintln!("PC: {}, PTR: {}, ", vm.pc, vm.offset + ptr);
+            eprintln!("PC: {}, PTR: {}, ", vm.pc, ptr);
         }
 
         InstOp::Add(val) => {
-            let ptr = (ptr + vm.offset) as usize;
             vm.memory[ptr] = vm.memory[ptr].wrapping_add(*val);
         }
         InstOp::Set(val) => {
-            let ptr = (ptr + vm.offset) as usize;
             vm.memory[ptr] = *val;
         }
 
         InstOp::MulAndSetZero(dests) => {
-            let source_ptr = (ptr + vm.offset) as usize;
-            let source_val = vm.memory[source_ptr];
+            let source_val = vm.memory[ptr];
             if source_val != 0 {
                 for (dest_p, m) in dests {
                     let dest_ptr = (*dest_p + vm.offset) as usize;
                     vm.memory[dest_ptr] = vm.memory[dest_ptr].wrapping_add(source_val.wrapping_mul(*m));
                 }
-                vm.memory[source_ptr] = 0;
+                vm.memory[ptr] = 0;
             }
         }
 
         InstOp::In => {
-            let ptr = (ptr + vm.offset) as usize;
             vm.memory[ptr] = input();
         }
         InstOp::Out => {
-            let ptr = (ptr + vm.offset) as usize;
             output(vm.memory[ptr]);
         }
 
         InstOp::LoopStart(end) => {
-            if vm.memory[(ptr + vm.offset) as usize] == 0 {
+            if vm.memory[ptr] == 0 {
                 vm.pc = *end;
             }
         }
         InstOp::LoopEnd(start) => {
-            if vm.memory[(ptr + vm.offset) as usize] != 0 {
+            if vm.memory[ptr] != 0 {
                 vm.pc = *start;
             }
         }
         InstOp::LoopEndWithOffset(start, off) => {
-            if vm.memory[(ptr + vm.offset) as usize] != 0 {
+            if vm.memory[ptr] != 0 {
                 vm.pc = *start;
             }
             vm.offset += off;
