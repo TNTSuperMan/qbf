0848e36813dd8f87d9719fa206e3eefac5433ee7
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-29 16:54:58 +0900

    色々wrapping_addにする

--- src/interpret.rs
+++ src/interpret.rs
@@ -9,7 +9,7 @@ pub fn run(insts: Vec<Instruction>, size: usize) {
     let mut memory: Vec<u8> = vec![0; size];
     loop {
         let Instruction { opcode, pointer } = &insts[pc];
-        let ptr = (pointer + offset) as usize;
+        let ptr = offset.wrapping_add(*pointer) as usize;
         match opcode {
             InstOp::Breakpoint => {
                 // 標準出力と分けるだけ、エラーじゃない
@@ -24,25 +24,25 @@ pub fn run(insts: Vec<Instruction>, size: usize) {
             }
 
             InstOp::Shift(diff) => {
-                while memory[(pointer + offset) as usize] != 0 {
-                    offset += diff;
+                while memory[offset.wrapping_add(*pointer) as usize] != 0 {
+                    offset = offset.wrapping_add(*diff);
                 }
             }
             InstOp::MulAndSetZero(dests) => {
                 let source_val = memory[ptr];
                 if source_val != 0 {
                     for (dest_p, m) in dests {
-                        let dest_ptr = (dest_p + offset) as usize;
+                        let dest_ptr = offset.wrapping_add(*dest_p) as usize;
                         memory[dest_ptr] = memory[dest_ptr].wrapping_add(source_val.wrapping_mul(*m));
                     }
                     memory[ptr] = 0;
                 }
             }
             InstOp::MulAndSetZeroTo(source, dests) => {
-                let source_val = memory[(source + offset) as usize].wrapping_add(memory[ptr]);
+                let source_val = memory[offset.wrapping_add(*source) as usize].wrapping_add(memory[ptr]);
                 if source_val != 0 {
                     for (dest_p, m) in dests {
-                        let dest_ptr = (dest_p + offset) as usize;
+                        let dest_ptr = offset.wrapping_add(*dest_p) as usize;
                         memory[dest_ptr] = memory[dest_ptr].wrapping_add(source_val.wrapping_mul(*m));
                     }
                     memory[ptr] = 0;
@@ -70,12 +70,12 @@ pub fn run(insts: Vec<Instruction>, size: usize) {
                 if memory[ptr] != 0 {
                     pc = *start;
                 }
-                offset += off;
+                offset = offset.wrapping_add(*off);
             }
             InstOp::End => {
                 break;
             }
         }
-        pc += 1;
+        pc = pc.wrapping_add(1);
     }
 }
