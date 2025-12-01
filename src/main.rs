f7251bb54fa8913ce7612a3837f6a0062e32e12b
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2025-12-01 17:59:03 +0900

    ベンチマーク用オプションを追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use crate::{bytecode::ir_to_bytecodes, interpret::run, ir::parse_to_ir, trace::OperationCountMap};
 use clap::Parser;
 
@@ -9,11 +11,14 @@ mod trace;
 #[derive(Parser, Debug)]
 #[command(name = "qbf")]
 struct Args {
+    #[arg(value_name = "FILE")]
+    file: String,
+
     #[arg(short, long, default_value_t = 65536)]
     memory_size: usize,
     
-    #[arg(value_name = "FILE")]
-    file: String,
+    #[arg(short, long)]
+    benchmark_count: Option<usize>,
 }
 
 fn main() {
@@ -24,20 +29,41 @@ fn main() {
             eprintln!("Error: {}", e);
         }
         Ok(code) => {
-            let ir = parse_to_ir(&code);
-            let bytecodes = ir_to_bytecodes(ir);
-            let mut v = OperationCountMap::new(bytecodes.len());
-            let result = run(bytecodes.clone(), args.memory_size, &mut v);
-            if let Err(err) = result.clone() {
-                eprintln!("{}", err);
-            }
-            #[cfg(feature = "debug")] {
-                use crate::trace::instructions_to_string;
-                use std::fs;
-                if let Ok(mem) = result {
-                    fs::write("./box/memory", mem).expect("failed to write");
+            if let Some(count) = args.benchmark_count {
+                let mut times: Vec<f64> = vec![];
+                for _ in 0..count {
+                    let start = Instant::now();
+
+                    let ir = parse_to_ir(&code);
+                    let bytecodes = ir_to_bytecodes(ir);
+                    let mut v = OperationCountMap::new(bytecodes.len());
+                    let result = run(bytecodes.clone(), args.memory_size, &mut v);
+                    if let Err(err) = result.clone() {
+                        eprintln!("{}", err);
+                        return;
+                    }
+
+                    times.push(start.elapsed().as_secs_f64());
                 }
-                fs::write("./box/bytecodes", instructions_to_string(bytecodes, v)).expect("failed to write");
+
+                let mean = times.iter().sum::<f64>() / times.len() as f64;
+                println!("Mean time(sec): {}", mean);
+            } else {
+                let ir = parse_to_ir(&code);
+                let bytecodes = ir_to_bytecodes(ir);
+                let mut v = OperationCountMap::new(bytecodes.len());
+                let result = run(bytecodes.clone(), args.memory_size, &mut v);
+                if let Err(err) = result.clone() {
+                    eprintln!("{}", err);
+                }
+                #[cfg(feature = "debug")] {
+                    use crate::trace::instructions_to_string;
+                    use std::fs;
+                    if let Ok(mem) = result {
+                        fs::write("./box/memory", mem).expect("failed to write");
+                    }
+                    fs::write("./box/bytecodes", instructions_to_string(bytecodes, v)).expect("failed to write");
+                }
             }
         }
     }
