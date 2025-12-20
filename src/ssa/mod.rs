27a5694033a8f6234d848068c82c358406adc433
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2025-12-20 13:06:11 +0900

    形変えたり

--- src/ssa/mod.rs
+++ src/ssa/mod.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, fmt::Debug};
 
 pub mod parse;
+mod inline;
 
 type PointerSSAHistory = HashMap<isize, Vec<PointerOperation>>;
 
@@ -12,7 +13,7 @@ pub struct PointerVersion {
 
 impl Debug for PointerVersion {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&format!("PointerVersion([{}]#{})", self.ptr, self.version))?;
+        f.write_str(&format!("[{}]#{}", self.ptr, self.version))?;
         Ok(())
     }
 }
@@ -21,21 +22,21 @@ impl Debug for PointerVersion {
 pub enum PointerOperation {
     UntrackedValue(isize),
     AssignConstant(u8),
-    AddConstant(u8),
+    AddConstant(PointerVersion, u8),
     AssignFromPointer(PointerVersion),
-    AddFromPointer(PointerVersion),
-    AddMultipliedValue(u8, PointerVersion),
+    AddFromPointer(PointerVersion, PointerVersion),
+    AddMultipliedValue(PointerVersion, PointerVersion, u8),
 }
 
 impl Debug for PointerOperation {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            PointerOperation::UntrackedValue(ptr) => f.write_str(&format!("UntrackedValue([{}])", ptr)),
-            PointerOperation::AssignConstant(val) => f.write_str(&format!("AssignConstant({})", val)),
-            PointerOperation::AddConstant(val) => f.write_str(&format!("AddConstant({})", val)),
-            PointerOperation::AssignFromPointer(version) => f.write_str(&format!("AssignFromPointer({:?})", version)),
-            PointerOperation::AddFromPointer(version) => f.write_str(&format!("AddFromPointer({:?})", version)),
-            PointerOperation::AddMultipliedValue(val, version) => f.write_str(&format!("AddMultipliedValue({},{:?})", val, version)),
+            PointerOperation::UntrackedValue(ptr) => f.write_str(&format!("raw [{}]", ptr)),
+            PointerOperation::AssignConstant(val) => f.write_str(&format!("const {}", val)),
+            PointerOperation::AddConstant(ptr, val) => f.write_str(&format!("$ac {:?} + {}", ptr, val)),
+            PointerOperation::AssignFromPointer(version) => f.write_str(&format!("$ld {:?}", version)),
+            PointerOperation::AddFromPointer(to, dest) => f.write_str(&format!("$afp {:?} + {:?}", to, dest)),
+            PointerOperation::AddMultipliedValue(to, dest, val) => f.write_str(&format!("$amv {:?} + {:?} * {}", to, dest, val)),
         }?;
         Ok(())
     }
--- src/ssa/parse.rs
+++ src/ssa/parse.rs
@@ -15,7 +15,10 @@ pub fn build_ssa_from_ir(ir_arr: &[IR]) -> Option<PointerSSAHistory> {
         match &ir.opcode {
             IROp::Add(val) => {
                 let current_history = get_pointer_history!(ir.pointer);
-                current_history.push(PointerOperation::AddConstant(*val));
+                current_history.push(PointerOperation::AddConstant(PointerVersion {
+                    ptr: ir.pointer,
+                    version: current_history.len() - 1,
+                }, *val));
             }
             IROp::Set(val) => {
                 let current_history = get_pointer_history!(ir.pointer);
@@ -31,7 +34,10 @@ pub fn build_ssa_from_ir(ir_arr: &[IR]) -> Option<PointerSSAHistory> {
                 };
                 for (dest_ptr, dest_val) in dests {
                     let dest_history = get_pointer_history!(*dest_ptr);
-                    dest_history.push(PointerOperation::AddMultipliedValue(*dest_val, source_version));
+                    dest_history.push(PointerOperation::AddMultipliedValue(PointerVersion {
+                        ptr: *dest_ptr,
+                        version: dest_history.len() - 1,
+                    }, source_version, *dest_val));
                 }
                 let source_history = get_pointer_history!(ir.pointer);
                 source_history.push(PointerOperation::AssignConstant(0));
