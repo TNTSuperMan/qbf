bd4a0589cb505e2adced16be4d660cced5027408
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-01-20 08:50:17 +0900

    デバッグ用の色々を戻す

--- src/cisc/interpret.rs
+++ src/cisc/interpret.rs
@@ -204,7 +204,7 @@ pub fn run(insts: &[Bytecode], memory: &mut Memory, ocm: &mut OperationCountMap)
             }
             OpCode::Out => {
                 pointer += bytecode.delta as isize;
-                //stdout.write(&[memory.get(pointer)?]).map_err(|_| "Runtime Error: Failed to print")?;
+                stdout.write(&[memory.get(pointer)?]).map_err(|_| "Runtime Error: Failed to print")?;
             }
 
             OpCode::JmpIfZero => {
@@ -224,7 +224,7 @@ pub fn run(insts: &[Bytecode], memory: &mut Memory, ocm: &mut OperationCountMap)
             OpCode::PositiveRangeCheckJNZ => {
                 pointer += bytecode.delta as isize;
                 if (bytecode.val as i8 as i16 as u16 as isize) <= pointer {
-                    println!("deoptP {} {}", bytecode.val as i8 as i16 as u16 as isize, pointer);
+                    // TODO: deopt
                 }
                 if memory.get(pointer)? != 0 {
                     pc = bytecode.addr as usize;
@@ -234,7 +234,7 @@ pub fn run(insts: &[Bytecode], memory: &mut Memory, ocm: &mut OperationCountMap)
             OpCode::NegativeRangeCheckJNZ => {
                 pointer += bytecode.delta as isize;
                 if (bytecode.val as i8 as i16 as u16 as isize) > pointer {
-                    println!("deoptN {} {}", bytecode.val as i8 as i16 as u16 as isize, pointer);
+                    // TODO: deopt
                 }
                 if memory.get(pointer)? != 0 {
                     pc = bytecode.addr as usize;
