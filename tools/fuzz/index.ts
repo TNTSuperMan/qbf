74b57272955aca5c94ce6e4f13c6f83a44945317
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-02-11 13:17:40 +0900

    タイムアウトの表示を変更

--- tools/fuzz/index.ts
+++ tools/fuzz/index.ts
@@ -45,7 +45,6 @@ while (true) {
     const code = GenerateRandomBFCode(INPUT_MINLEN, INPUT_MAXLEN);
     const exec_result = execute({ code, timeout_cycles: MAX_STEPS });
     if (exec_result.type !== "timeout") {
-        stdout.write(".");
         const brainrot_process = spawn({
             cmd: [QBF_FILE, "/dev/stdin"],
             stdin: new Blob([code]),
@@ -54,8 +53,10 @@ while (true) {
         const race_res = await Promise.race([brainrot_process.exited, sleep(TIMEOUT_MS)]);
         if (race_res === undefined) { // sleep(TIMEOUT_MS)の挙動
             brainrot_process.kill();
+            stdout.write("_");
             //await report(code, "timeout");
         } else {
+            stdout.write(".");
             const stderr = await brainrot_process.stderr.text();
             switch (race_res) {
                 case 0: // Expected behavior in fuzzing
