0efe441f161b9ea8d90dfeed00708ab1d004c999
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-02-13 14:48:40 +0900

    interpret_optでデリファレンス

--- src/cisc/interpret_opt.rs
+++ src/cisc/interpret_opt.rs
@@ -19,41 +19,41 @@ pub unsafe fn run_opt(vm: &mut UnsafeVM) -> Result<InterpreterResult, String> {
         
         match vm.get_op() {
             Bytecode::Breakpoint { delta } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 eprintln!("PC: {}, PTR: {}", vm.get_pc(), vm.get_ptr());
             }
 
             Bytecode::SingleAdd { delta, val } => {
-                vm.step_ptr(delta as isize);
-                vm.add(val);
+                vm.step_ptr((*delta) as isize);
+                vm.add(*val);
             }
             Bytecode::SingleSet { delta, val } => {
-                vm.step_ptr(delta as isize);
-                vm.set(val);
+                vm.step_ptr((*delta) as isize);
+                vm.set(*val);
             }
             Bytecode::AddAdd { delta1, val1, delta2, val2 } => {
-                vm.step_ptr(delta1 as isize);
-                vm.add(val1);
-                vm.step_ptr(delta2 as isize);
-                vm.add(val2);
+                vm.step_ptr((*delta1) as isize);
+                vm.add(*val1);
+                vm.step_ptr((*delta2) as isize);
+                vm.add(*val2);
             }
             Bytecode::AddSet { delta1, val1, delta2, val2 } => {
-                vm.step_ptr(delta1 as isize);
-                vm.add(val1);
-                vm.step_ptr(delta2 as isize);
-                vm.set(val2);
+                vm.step_ptr((*delta1) as isize);
+                vm.add(*val1);
+                vm.step_ptr((*delta2) as isize);
+                vm.set(*val2);
             }
             Bytecode::SetAdd { delta1, val1, delta2, val2 } => {
-                vm.step_ptr(delta1 as isize);
-                vm.set(val1);
-                vm.step_ptr(delta2 as isize);
-                vm.add(val2);
+                vm.step_ptr((*delta1) as isize);
+                vm.set(*val1);
+                vm.step_ptr((*delta2) as isize);
+                vm.add(*val2);
             }
             Bytecode::SetSet { delta1, val1, delta2, val2 } => {
-                vm.step_ptr(delta1 as isize);
-                vm.set(val1);
-                vm.step_ptr(delta2 as isize);
-                vm.set(val2);
+                vm.step_ptr((*delta1) as isize);
+                vm.set(*val1);
+                vm.step_ptr((*delta2) as isize);
+                vm.set(*val2);
             }
 
             Bytecode::BothRangeCheck { range } => {
@@ -63,15 +63,15 @@ pub unsafe fn run_opt(vm: &mut UnsafeVM) -> Result<InterpreterResult, String> {
                 }
             }
             Bytecode::Shift { delta, step } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
             }
             Bytecode::ShiftP { delta, step, range } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
                 if !range.contains(&(vm.get_ptr() as u16)) {
                     vm.jump_one();
@@ -79,9 +79,9 @@ pub unsafe fn run_opt(vm: &mut UnsafeVM) -> Result<InterpreterResult, String> {
                 }
             }
             Bytecode::ShiftN { delta, step, range } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
                 if !range.contains(&(vm.get_ptr() as u16)) {
                     vm.jump_one();
@@ -89,83 +89,83 @@ pub unsafe fn run_opt(vm: &mut UnsafeVM) -> Result<InterpreterResult, String> {
                 }
             }
             Bytecode::ShiftAdd { delta1, step, delta2, val } => {
-                vm.step_ptr(delta1 as isize);
+                vm.step_ptr((*delta1) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
-                vm.step_ptr(delta2 as isize);
-                vm.add(val);
+                vm.step_ptr((*delta2) as isize);
+                vm.add(*val);
             }
             Bytecode::ShiftAddP { delta1, step, delta2, val, range } => {
-                vm.step_ptr(delta1 as isize);
+                vm.step_ptr((*delta1) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
                 if !range.contains(&(vm.get_ptr() as u16)) {
-                    vm.step_ptr(delta2 as isize);
-                    vm.inner.memory.add(vm.get_ptr(), val)?;
+                    vm.step_ptr((*delta2) as isize);
+                    vm.inner.memory.add(vm.get_ptr(), *val)?;
                     vm.jump_one();
                     return Ok(InterpreterResult::ToggleTier(Tier::Deopt));
                 }
-                vm.step_ptr(delta2 as isize);
-                vm.add(val);
+                vm.step_ptr((*delta2) as isize);
+                vm.add(*val);
             }
             Bytecode::ShiftAddN { delta1, step, delta2, val, range } => {
-                vm.step_ptr(delta1 as isize);
+                vm.step_ptr((*delta1) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
                 if !range.contains(&(vm.get_ptr() as u16)) {
-                    vm.step_ptr(delta2 as isize);
-                    vm.inner.memory.add(vm.get_ptr(), val)?;
+                    vm.step_ptr((*delta2) as isize);
+                    vm.inner.memory.add(vm.get_ptr(), *val)?;
                     vm.jump_one();
                     return Ok(InterpreterResult::ToggleTier(Tier::Deopt));
                 }
-                vm.step_ptr(delta2 as isize);
-                vm.add(val);
+                vm.step_ptr((*delta2) as isize);
+                vm.add(*val);
             }
             Bytecode::ShiftSet { delta1, step, delta2, val } => {
-                vm.step_ptr(delta1 as isize);
+                vm.step_ptr((*delta1) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
-                vm.step_ptr(delta2 as isize);
-                vm.set(val);
+                vm.step_ptr((*delta2) as isize);
+                vm.set(*val);
             }
             Bytecode::ShiftSetP { delta1, step, delta2, val, range } => {
-                vm.step_ptr(delta1 as isize);
+                vm.step_ptr((*delta1) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
                 if !range.contains(&(vm.get_ptr() as u16)) {
-                    vm.step_ptr(delta2 as isize);
-                    vm.inner.memory.set(vm.get_ptr(), val)?;
+                    vm.step_ptr((*delta2) as isize);
+                    vm.inner.memory.set(vm.get_ptr(), *val)?;
                     vm.jump_one();
                     return Ok(InterpreterResult::ToggleTier(Tier::Deopt));
                 }
-                vm.step_ptr(delta2 as isize);
-                vm.set(val);
+                vm.step_ptr((*delta2) as isize);
+                vm.set(*val);
             }
             Bytecode::ShiftSetN { delta1, step, delta2, val, range } => {
-                vm.step_ptr(delta1 as isize);
+                vm.step_ptr((*delta1) as isize);
                 while vm.inner.memory.get(vm.get_ptr())? != 0 {
-                    vm.step_ptr(step as isize);
+                    vm.step_ptr((*step) as isize);
                 }
                 if !range.contains(&(vm.get_ptr() as u16)) {
-                    vm.step_ptr(delta2 as isize);
-                    vm.inner.memory.set(vm.get_ptr(), val)?;
+                    vm.step_ptr((*delta2) as isize);
+                    vm.inner.memory.set(vm.get_ptr(), *val)?;
                     vm.jump_one();
                     return Ok(InterpreterResult::ToggleTier(Tier::Deopt));
                 }
-                vm.step_ptr(delta2 as isize);
-                vm.set(val);
+                vm.step_ptr((*delta2) as isize);
+                vm.set(*val);
             }
 
             Bytecode::MulStart { delta, jz_abs } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 let val = vm.get();
                 if val == 0 {
-                    vm.jump_abs(jz_abs);
+                    vm.jump_abs(*jz_abs);
                     continue;
                 } else {
                     mul_val = val;
@@ -173,54 +173,54 @@ pub unsafe fn run_opt(vm: &mut UnsafeVM) -> Result<InterpreterResult, String> {
                 }
             }
             Bytecode::Mul { delta, val } => {
-                vm.add_with_offset(delta as isize, mul_val.wrapping_mul(val));
+                vm.add_with_offset((*delta) as isize, mul_val.wrapping_mul(*val));
             }
 
             Bytecode::SingleMoveAdd { delta, to } => {
-                vm.step_ptr(delta as isize);
-                vm.add_with_offset(to as isize, vm.get());
+                vm.step_ptr((*delta) as isize);
+                vm.add_with_offset((*to) as isize, vm.get());
                 vm.set(0);
             }
             Bytecode::SingleMoveSub { delta, to } => {
-                vm.step_ptr(delta as isize);
-                vm.sub_with_offset(to as isize, vm.get());
+                vm.step_ptr((*delta) as isize);
+                vm.sub_with_offset((*to) as isize, vm.get());
                 vm.set(0);
             }
 
             Bytecode::DoubleMoveAddAdd { delta, to1, to2 } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 let v = vm.get();
-                vm.add_with_offset(to1 as isize, v);
-                vm.add_with_offset(to2 as isize, v);
+                vm.add_with_offset((*to1) as isize, v);
+                vm.add_with_offset((*to2) as isize, v);
                 vm.set(0);
             }
             Bytecode::DoubleMoveAddSub { delta, to1, to2 } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 let v = vm.get();
-                vm.add_with_offset(to1 as isize, v);
-                vm.sub_with_offset(to2 as isize, v);
+                vm.add_with_offset((*to1) as isize, v);
+                vm.sub_with_offset((*to2) as isize, v);
                 vm.set(0);
             }
             Bytecode::DoubleMoveSubAdd { delta, to1, to2 } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 let v = vm.get();
-                vm.sub_with_offset(to1 as isize, v);
-                vm.add_with_offset(to2 as isize, v);
+                vm.sub_with_offset((*to1) as isize, v);
+                vm.add_with_offset((*to2) as isize, v);
                 vm.set(0);
             }
             Bytecode::DoubleMoveSubSub { delta, to1, to2 } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 let v = vm.get();
-                vm.sub_with_offset(to1 as isize, v);
-                vm.sub_with_offset(to2 as isize, v);
+                vm.sub_with_offset((*to1) as isize, v);
+                vm.sub_with_offset((*to2) as isize, v);
                 vm.set(0);
             }
 
             Bytecode::MoveStart { delta, jz_abs } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 let val = vm.get();
                 if val == 0 {
-                    vm.jump_abs(jz_abs);
+                    vm.jump_abs(*jz_abs);
                     continue;
                 } else {
                     mul_val = val;
@@ -228,21 +228,21 @@ pub unsafe fn run_opt(vm: &mut UnsafeVM) -> Result<InterpreterResult, String> {
                 }
             }
             Bytecode::MoveAdd { delta } => {
-                vm.add_with_offset(delta as isize, mul_val);
+                vm.add_with_offset((*delta) as isize, mul_val);
             }
             Bytecode::MoveSub { delta } => {
-                vm.sub_with_offset(delta as isize, mul_val);
+                vm.sub_with_offset((*delta) as isize, mul_val);
             }
 
             Bytecode::In { delta } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 match stdin.read_exact(&mut stdin_buf) {
                     Ok(_) => vm.set(stdin_buf[0]),
                     Err(_) => vm.set(0),
                 }
             }
             Bytecode::Out { delta } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 stdout.write(&[vm.get()]).map_err(|_| "Runtime Error: Failed to print")?;
                 if vm.inner.flush {
                     stdout.flush().map_err(|_| "Runtime Error: Failed to flush")?;
@@ -250,68 +250,68 @@ pub unsafe fn run_opt(vm: &mut UnsafeVM) -> Result<InterpreterResult, String> {
             }
 
             Bytecode::JmpIfZero { delta, addr_abs } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 if vm.get() == 0 {
-                    vm.jump_abs(addr_abs);
+                    vm.jump_abs(*addr_abs);
                     continue;
                 }
             }
             Bytecode::JmpIfNotZero { delta, addr_abs } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 if vm.get() != 0 {
-                    vm.jump_abs(addr_abs);
+                    vm.jump_abs(*addr_abs);
                     continue;
                 }
             }
             Bytecode::PositiveRangeCheckJNZ { delta, addr_back, range } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 if !range.contains(&(vm.get_ptr() as u16)) {
                     if vm.inner.memory.get(vm.get_ptr())? != 0 {
-                        vm.jump_back(addr_back);
+                        vm.jump_back(*addr_back);
                     } else {
                         vm.jump_one();
                     }
                     return Ok(InterpreterResult::ToggleTier(Tier::Deopt));
                 }
                 if vm.get() != 0 {
-                    vm.jump_back(addr_back);
+                    vm.jump_back(*addr_back);
                     continue;
                 }
             }
             Bytecode::NegativeRangeCheckJNZ { delta, addr_back, range } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 if !range.contains(&(vm.get_ptr() as u16)) {
                     if vm.inner.memory.get(vm.get_ptr())? != 0 {
-                        vm.jump_back(addr_back);
+                        vm.jump_back(*addr_back);
                     } else {
                         vm.jump_one();
                     }
                     return Ok(InterpreterResult::ToggleTier(Tier::Deopt));
                 }
                 if vm.get() != 0 {
-                    vm.jump_back(addr_back);
+                    vm.jump_back(*addr_back);
                     continue;
                 }
             }
             Bytecode::BothRangeCheckJNZ { delta, addr_back, range } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 let ptr = vm.get_ptr();
                 if !range.contains(&(ptr as u16)) {
                     if vm.inner.memory.get(ptr)? != 0 {
-                        vm.jump_back(addr_back);
+                        vm.jump_back(*addr_back);
                     } else {
                         vm.jump_one();
                     }
                     return Ok(InterpreterResult::ToggleTier(Tier::Deopt));
                 }
                 if vm.get() != 0 {
-                    vm.jump_back(addr_back);
+                    vm.jump_back(*addr_back);
                     continue;
                 }
             }
 
             Bytecode::End { delta } => {
-                vm.step_ptr(delta as isize);
+                vm.step_ptr((*delta) as isize);
                 return Ok(InterpreterResult::End);
             }
         }
