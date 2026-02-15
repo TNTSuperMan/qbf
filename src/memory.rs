2b04fccaf811060a87980ced4f9efc0aa5b7a4c4
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-02-15 12:15:02 +0900

    そもそもMemoryでunsafe操作を行わない

--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,4 @@
-use anyhow::{Result, bail, ensure};
+use anyhow::{Context, Result};
 
 const MEMORY_LENGTH: usize = 65536;
 
@@ -12,32 +12,21 @@ impl Memory {
 
 impl Memory {
     pub fn get(&self, index: usize) -> Result<u8> {
-        match self.0.get(index) {
-            Some(val) => Ok(*val),
-            None => bail!("Runtime Error: Out of range memory get, Address: {}", index),
-        }
+        Ok(*self.0.get(index).with_context(|| format!("Runtime Error: Out of range memory read, Address: {index}"))?)
     }
     pub fn set(&mut self, index: usize, value: u8) -> Result<()> {
-        ensure!(self.0.len() > index, "Runtime Error: Out of range memory set, Address: {index}");
-        unsafe { // SAFETY: 直前に範囲を確認済み
-            *self.0.as_mut_ptr().add(index) = value;
-        }
+        let mem = self.0.get_mut(index).with_context(|| format!("Runtime Error: Out of range memory write, Address: {index}"))?;
+        *mem = value;
         Ok(())
     }
     pub fn add(&mut self, index: usize, value: u8) -> Result<()> {
-        ensure!(self.0.len() > index, "Runtime Error: Out of range memory add, Address: {index}");
-        unsafe { // SAFETY: 直前に範囲を確認済み
-            let ptr = self.0.as_mut_ptr().add(index);
-            *ptr = (*ptr).wrapping_add(value);
-        }
+        let mem = self.0.get_mut(index).with_context(|| format!("Runtime Error: Out of range memory add, Address: {index}"))?;
+        *mem = mem.wrapping_add(value);
         Ok(())
     }
     pub fn sub(&mut self, index: usize, value: u8) -> Result<()> {
-        ensure!(self.0.len() > index, "Runtime Error: Out of range memory sub, Address: {index}");
-        unsafe { // SAFETY: 直前に範囲を確認済み
-            let ptr = self.0.as_mut_ptr().add(index);
-            *ptr = (*ptr).wrapping_sub(value);
-        }
+        let mem = self.0.get_mut(index).with_context(|| format!("Runtime Error: Out of range memory sub, Address: {index}"))?;
+        *mem = mem.wrapping_sub(value);
         Ok(())
     }
 }
