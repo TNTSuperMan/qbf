368eed691cef6113e69976294a1cdf32908c07bb
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-02-16 14:45:28 +0900

    エラーメッセージとかを改良

--- src/cisc/error.rs
+++ src/cisc/error.rs
@@ -19,17 +19,17 @@ pub enum OptimizationError {
 
 #[derive(Error, Debug)]
 pub enum RuntimeError {
-    #[error("RuntimeError: Out of bound memory get, address: {0}")]
+    #[error("RuntimeError: Out of bounds while getting cell {0}")]
     OOBGet(usize),
     
-    #[error("RuntimeError: Out of bound memory set, address: {0}")]
-    OOBSet(usize),
+    #[error("RuntimeError: Out of bounds while setting {1} to cell {0}")]
+    OOBSet(usize, u8),
     
-    #[error("RuntimeError: Out of bound memory add, address: {0}")]
-    OOBAdd(usize),
+    #[error("RuntimeError: Out of bounds while adding {1} to cell {0}")]
+    OOBAdd(usize, u8),
     
-    #[error("RuntimeError: Out of bound memory sub, address: {0}")]
-    OOBSub(usize),
+    #[error("RuntimeError: Out of bounds while subtracting {1} to cell {0}")]
+    OOBSub(usize, u8),
 
     #[error("RuntimeError: {0}")]
     IOError(#[from] io::Error)
