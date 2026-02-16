c048db58687b938c0dca7d8083638f4177b9b55f
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-02-16 14:55:46 +0900

    エラーメッセージ改定

--- src/cisc/error.rs
+++ src/cisc/error.rs
@@ -4,33 +4,33 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum OptimizationError {
-    #[error("OptimizationError: Pointer delta overflow")]
+    #[error("Pointer delta overflow")]
     Delta(TryFromIntError),
 
-    #[error("OptimizationError: Shift step overflow")]
+    #[error("Shift step overflow")]
     ShiftStep(TryFromIntError),
 
-    #[error("OptimizationError: Program address overflow")]
+    #[error("Program address overflow")]
     ProgramAbs(TryFromIntError),
 
-    #[error("OptimizationError: Program relative address overflow")]
+    #[error("Program relative address overflow")]
     ProgramRel(TryFromIntError),
 }
 
 #[derive(Error, Debug)]
 pub enum RuntimeError {
-    #[error("RuntimeError: Out of bounds while getting cell {0}")]
+    #[error("Out of bounds while getting cell {0}")]
     OOBGet(usize),
     
-    #[error("RuntimeError: Out of bounds while setting {1} to cell {0}")]
+    #[error("Out of bounds while setting {1} to cell {0}")]
     OOBSet(usize, u8),
     
-    #[error("RuntimeError: Out of bounds while adding {1} to cell {0}")]
+    #[error("Out of bounds while adding {1} to cell {0}")]
     OOBAdd(usize, u8),
     
-    #[error("RuntimeError: Out of bounds while subtracting {1} to cell {0}")]
+    #[error("Out of bounds while subtracting {1} to cell {0}")]
     OOBSub(usize, u8),
 
-    #[error("RuntimeError: {0}")]
+    #[error("{0}")]
     IOError(#[from] io::Error)
 }
