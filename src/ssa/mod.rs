572db99a4435d3c1978efdd2a9b4294f099dd019
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-02-16 20:52:58 +0900

    optimize_loop関数のプロトタイプ

--- src/ssa/mod.rs
+++ src/ssa/mod.rs
@@ -1,5 +1,28 @@
+use crate::{ir::IR, ssa::{inline::inline_ssa_history, r#loop::{detect_ssa_loop, try_2step_loop}, parse::build_ssa_from_ir}};
+
 pub mod structs;
 pub mod parse;
 pub mod inline;
 pub mod to_ir;
 pub mod r#loop;
+
+pub fn optimize_loop(children: &[IR], start_pc: usize, loop_pointer: isize) -> Result<Vec<IR>, ()> {
+    let children_ssa = build_ssa_from_ir(children).ok_or(())?;
+
+    let inlined_children_ssa = inline_ssa_history(&children_ssa, false);
+
+    if let Some((loop_ptr, loop_ssa)) = detect_ssa_loop(&inlined_children_ssa) {
+        if loop_ptr != loop_pointer {
+            return Err(());
+        }
+
+        if let Some((second_loop_ssa, const_cells)) = try_2step_loop(&loop_ssa) {
+            todo!();
+        } else {
+            todo!();
+        }
+    }
+    todo!();
+
+    Err(())
+}
